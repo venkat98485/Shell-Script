82daac38e828dce7ce9ca9678c65c07f8a615dce
venkat <venky995930@gmail.com>
2025-08-02 11:15:06 +0530

    variable addded

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
-Person1=Trump
-Person2=Musk
+! /bin/bash
+Person1=$1
+Person2=$2
 echo "$Person1: Hey Musk How are you"
 echo "$Person2: Hey $Person1 i am fine, what about you"
 echo "$Person1: i am good whats up"
