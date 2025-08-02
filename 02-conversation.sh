8bdd6de6da3dd1d36cf5a2a01b370175d7196ce7
venkat <venky995930@gmail.com>
2025-08-02 10:44:29 +0530

    conversation added

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Mask How are you"
+echo "Musk: Hey Trump i am fine, what about you"
+echo "Trump: i am good whats up"
+echo "Musk: i am going to mars, will you come"
+echo "Trump: no, please carry on"
