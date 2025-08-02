ab08d0b27800dc94c320351d359d7a52ba9ebd9c
venkat <venky995930@gmail.com>
2025-08-02 10:53:35 +0530

    variable addded

---
+++ 03-conversation.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+Person1=Trump
+Person2=Musk
+
+echo "$Person1: Hey Musk How are you"
+echo "$Person2: Hey $Person1 i am fine, what about you"
+echo "$Person1: i am good whats up"
+echo "$Person2: i am going to mars, will you come"
+echo "$Person1: no, please carry on"
